6e8ec2ba09e58b850e98e09e98d916bd5492b9b3
Evecimar <evecimar@hotmail.com>
2015-07-02 04:28:54 -0300

    Auto modificador de acesso

---
+++ modificar.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+chmod 777 /var/www/html/xfood/upload/system/cache/
+chmod 777 /var/www/html/xfood/upload/system/logs/
+chmod 777 /var/www/html/xfood/upload/system/download/
+chmod 777 /var/www/html/xfood/upload/system/upload/
+chmod 777 /var/www/html/xfood/upload/image/
+chmod 777 /var/www/html/xfood/upload/image/cache/
+chmod 777 /var/www/html/xfood/upload/image/catalog/
+chmod 777 /var/www/html/xfood/upload/system/modification/
